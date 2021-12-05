24ec332b8bfd366778bcfc8893752bfa89aece55
jojo <jojo22123770@gmail.com>
2021-12-05 09:35:18 +0800

    Initial Commit

---
+++ homewprk3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  homewprk3
+//
+//  Created by User12 on 2021/12/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ homewprk3/homewprk3App.swift
@@ -0,0 +1,17 @@
+//
+//  homewprk3App.swift
+//  homewprk3
+//
+//  Created by User12 on 2021/12/5.
+//
+
+import SwiftUI
+
+@main
+struct homewprk3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
